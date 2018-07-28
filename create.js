9ff4edce4fbf8bc26da73dfb9c0289270e6a4081
Jorge Herrera <jorge@Jorges-MacBook-Pro.local>
2018-07-28 18:29:34 -0500

    Refactored code to use async await calls to dynamodb api

--- create.js
+++ create.js
@@ -1,9 +1,8 @@
 import uuid from "uuid";
+import * as dynamoDbLib from "./lib/dynamodb";
+import {success, failure} from "./lib/response";
 
-AWS.config.update({region: "us-east-1"});
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
-export function main(event, context, callback) {
+export async function main(event, context, callback) {
   // Request body is passed in as a JSON encoded string in 'event.body'
   const data = JSON.parse(event.body);
 
@@ -23,31 +22,10 @@ export function main(event, context, callback) {
     }
   };
 
-  dynamoDb.put(params, (error, date) => {
-    // Set response headers to enable CORS (Cross-Origin Resource Sharing)
-    const headers = {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": true
-    };
-
-    // Return status code 500 on error
-    if (error) {
-      const response = {
-        statusCode: 500,
-        headers: headers,
-        body: JSON.stringify({status: false})
-      };
-      callback(null, response);
-      return;
-    }
-
-    // Return status code 200 and the newly created item
-    const response = {
-      statusCode: 200,
-      headers: headers,
-      body: JSON.stringify(params.Item)
-    };
-
-    callback(null, response);
-  });
+  try {
+    await dynamoDbLib.call("put", params);
+    callback(null, success(params.Item));
+  } catch (e) {
+    callback(null, failure({status: false}));
+  }
 };
